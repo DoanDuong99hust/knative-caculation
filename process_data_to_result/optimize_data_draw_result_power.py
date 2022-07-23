75d2a3136ad273891094ca344e53bdad8855110c
DoanDuong99hust <doanduong1999@gmail.com>
2022-07-23 14:11:28 +0700

    draw fitting result

--- process_data_to_result/optimize_data_draw_result_power.py
+++ process_data_to_result/optimize_data_draw_result_power.py
@@ -1,14 +1,14 @@
-from cProfile import label
-from cmath import sin
 from collections import defaultdict
 import csv
-import math
+import datetime
 from tkinter.font import names
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 import os
 import re
+
+from pytz import utc
 
 TARGET_PODS = "target_pods"
 REPEAT_TIME = "repeat_time"
@@ -19,12 +19,19 @@ POD_NUM = "pod_num"
 CPU_PER = "cpu_%"
 RAM_PER = "ram_%"
 POD_STATE = "pod_state"
+# Raw power data
+TIME = "time"
+VOLT = "Volts"
+AMPE = "Amps"
+WATT ="Watts"
 
-folder = "pi4"
-CALCULATION_TYPE = "revert_lifecircle"
 data_by_day_folder = "20_7_2022"
-data_directory = "D:\FIL\DATN\Data\\"+folder
-output_data_dir = "D:\FIL\DATN\DataOptimize\\"+folder+"\\"
+power_with_status_folder = "power_with_status" 
+data_directory = "D:\FIL\DATN\Data\pi4\\" + data_by_day_folder
+CALCULATION_TYPE = "revert_lifecircle"
+power_data_directory = "D:\FIL\DATN\Data\pi4\power\\" + data_by_day_folder + "\\" +CALCULATION_TYPE
+output_data_dir = "D:\FIL\DATN\DataOptimize\\pi4\\"
+
 data_optimize_file = output_data_dir+"data_{}_{}_optimize_tmp.csv"
 
 def remove_optimize_file(file_directory:str):
@@ -34,7 +41,7 @@ def remove_optimize_file(file_directory:str):
         print("The file does not exist")
 
 def get_dic_data(file_directory:str, data_type:str):
-    data_pd = pd.read_csv(file_directory, sep=',', names=[TIME_SECONDS, DATE_TIME, POD_NUM, CPU_PER, RAM_PER, POD_STATE])
+    data_pd = pd.read_csv(file_directory, sep=',', names=[TIME, VOLT, AMPE, WATT, POD_STATE])
     data_arr = np.transpose(np.array([data_pd[POD_STATE], data_pd[data_type]]))
     dict_data = defaultdict(list)
     for key, value in data_arr:
@@ -44,82 +51,38 @@ def get_dic_data(file_directory:str, data_type:str):
         data_mean_dic[key].append(sum(value)/len(value))
     return data_mean_dic
 
-def get_state_mean_dic_data(file_directory:str):
-    df = pd.read_csv(file_directory, sep=',', names=[TARGET_PODS, REPEAT_TIME, POD_STATE, MEAN])
-    data_arr = df.to_numpy()
-    state_dic = defaultdict(list)
-    for target_pod, repeat_time, pod_state, mean in data_arr:
-        state_dic[pod_state].append([target_pod, repeat_time, mean])
-    state_mean_dic = defaultdict(list)
-    for key, value in state_dic.items():
-        repeat_mean_dic = defaultdict(list)
-        for target_pod, rep, mean in value:
-            repeat_mean_dic[target_pod].append(mean)
-        mean_dic = defaultdict(list)
-        for k, v in repeat_mean_dic.items():
-            mean_dic[k].append(sum(v)/len(v))
-        state_mean_dic[key].append(mean_dic)
-    return state_mean_dic
-
-def write_to_file(calculation_type:str ,data_type:str, target_pod:str, repeat_time:str, key:str, value:float):
-    write = csv.writer(open(data_optimize_file.format(data_type,calculation_type), 'a', newline=''))
+def get_mean_dic(file_directory:str, data_type:str):
+    data_pd = pd.read_csv(file_directory, sep=',', names=[TIME, VOLT, AMPE, WATT, POD_STATE])
+    data_arr = np.transpose(np.array([data_pd[POD_STATE], data_pd[TIME], data_pd[data_type]]))
+    dict_data1 = defaultdict(list)
+    for key, value1,value2 in data_arr:
+        dict_data1[key].append([format_time(str(value1)).timestamp(),value2])
+    data_mean_dic1 = defaultdict(list)
+    for key, value in dict_data1.items():
+        total_value = 0
+        for index in range(1,len(value),1):
+            total_value = total_value + value[index-1][1]*(value[index][0]-value[index-1][0]) # 
+        data_mean_dic1[key].append(total_value/(value[len(value)-1][0]-value[0][0]))
+    return data_mean_dic1
+    
+def write_to_file(data_type:str, target_pod:str, repeat_time:str, key:str, value:float):
+    write = csv.writer(open(data_optimize_file.format(data_type, CALCULATION_TYPE), 'a', newline=''))
     write.writerow([target_pod,repeat_time,key,value])
 
-def generate_optimize_data_files(data_directory:str, calculation_type:str):
-    for filename in os.listdir(data_directory+"\\"+calculation_type):
-        file_directory = os.path.join(data_directory+"\\"+calculation_type, filename)
-        print(file_directory)
+def generate_optimize_data_files(data_directory:str):
+    for filename in os.listdir(data_directory):
+        file_directory = os.path.join(data_directory, filename)
         # checking if it is a file
         if os.path.isfile(file_directory):
             target_pod = re.search('target_pod_(.*)_repeat',file_directory).group(1)
             repeat_time = re.search('repeat_time_(.*)_video',file_directory).group(1)[0]
             print("file_directory: ", file_directory, "target_pod: ", target_pod, "repeat_time: ", repeat_time)
-            for key, value in get_dic_data(file_directory, CPU_PER).items():
-                write_to_file(calculation_type, CPU_PER,target_pod,repeat_time,key,float(value[0]))
+            for key, value in get_dic_data(file_directory,WATT).items():
+                write_to_file("power",target_pod,repeat_time,key,float(value[0]))
                 # data_cpu_optimize.append([target_pod,repeat_time,key,str(value[0])])
-            for key, value in get_dic_data(file_directory, RAM_PER).items():
-                write_to_file(calculation_type, RAM_PER,target_pod,repeat_time,key,float(value[0]))
-                # data_ram_optimize.append([target_pod,repeat_time,key,str(value[0])])
 
-def mean_plot(data_type:str):
-    dic = get_state_mean_dic_data(output_data_dir+"data_{}_optimize_tmp.csv".format(data_type))
-    # state
-    null_state = dic.get("begin")[0]
-    warm_state = dic.get("warm")[0]
-    cold_state = dic.get("cold")[0]
-    active_state = dic.get("active")[0]
-
-    state_arr = [[null_state.keys(), null_state.values()], [warm_state.keys(), warm_state.values()], [cold_state.keys(), cold_state.values()], [active_state.keys(), active_state.values()]]
-    color = ["red", "orange", "green", "blue"]
-    marker = ["v", ">", "s", "v",]
-    labels = ["State:Null", "State:Warm", "State:Cold", "State:Active", ]
-
-    x = np.arange(1,11,1)
-
-    for i in range(len(state_arr)):
-        x = np.array(list(state_arr[i][0]))
-        y = np.array(list(state_arr[i][1]))
-        print(x, y)
-        a,b=np.polyfit(x,y,1)
-
-        plt.scatter(x, y, color=color[i], marker=marker[i], label = labels[i])
-        plt.plot(x,a*x+b, color=color[i])
-        plt.yscale("log", base=2)
-        plt.xticks(x)
-        plt.xlabel("Number of Pods")
-        plt.ylabel("{} state {} usage (log2)".format(folder,data_type))
-    # plt.scatter(dic.keys()[0],dic.values())
-    # plt.scatter(null_state.keys(), null_state.values(), label="State:Null")
-    # plt.scatter(warm_state.keys(), warm_state.values(), label="State:Warm")
-    # plt.scatter(cold_state.keys(), cold_state.values(), label="State:Cold")
-    # plt.scatter(active_state.keys(), active_state.values(), label="State:Active")
-
-    plt.legend()
-    plt.show()
-
-
-def boxplot_state(data_type:str, calculation_type:str):
-    df = pd.read_csv(output_data_dir+"data_{}_{}_optimize_tmp.csv".format(data_type, calculation_type), sep=',', names=[TARGET_PODS, REPEAT_TIME, POD_STATE, MEAN])
+def boxplot_state(data_type:str):
+    df = pd.read_csv(output_data_dir+"data_{}_{}_optimize_tmp.csv".format(data_type, CALCULATION_TYPE), sep=',', names=[TARGET_PODS, REPEAT_TIME, POD_STATE, MEAN])
     # df = pd.DataFrame(arr_data,columns=[TARGET_PODS, REPEAT_TIME, POD_STATE, MEAN])
     df = df.sort_values(by=TARGET_PODS)
 
@@ -137,14 +100,14 @@ def boxplot_state(data_type:str, calculation_type:str):
 
     plt.figure()
 
-    arr = [df_null, df_warm, df_cold, df_active]
-    # print(arr)
+    arr = [df_null, df_warm, df_cold]
+    print(arr)
     i=0
     color = ["red", "orange", "green", "blue"]
 
     marker = ["v", ">", "s", "v",]
 
-    labels = ["State:Null", "State:Warm", "State:Cold", "State:Active", ]
+    labels = ["State:Null", "State:Warm", "State:Cold", ]
 
     # color = ["red", "orange", "yellow", "green", "blue", "violet", "black"]
 
@@ -156,29 +119,20 @@ def boxplot_state(data_type:str, calculation_type:str):
         # plt.subplot(1, 1, i+1)
         plt.subplot(1, 1, 1)
 
-        draw_box = scenario[[TARGET_PODS, MEAN]]
         
         list_pods = np.unique(df[TARGET_PODS])
         ticks= [str(e) for e in list_pods]
         x_positions = list(range(0, len(ticks)))
 
-        # for pod in list_pods:
-        #     draw_i = draw_box[draw_box[TARGET_PODS] == pod]
-
-        #     if(len(draw_i) != 0):
-        #         plt.boxplot(draw_i[MEAN], positions=[ticks.index(str(pod))], widths=0.5, patch_artist=True, boxprops=dict(facecolor='white'))
-                
         plt.xticks(x_positions, ticks)
 
         plt.xlabel("Number of Pods")
-        plt.yscale("log",base=2)
-        plt.ylabel("{} state {} usage".format(folder,data_type))
+        # plt.yscale("log", base=10)
+        plt.ylabel("PI4 state {} usage Watts".format(data_type))
         
         if i==0:xx=1
         elif i==1: xx=1
         elif i==2: xx=2
-        elif i==3: xx=3
-        # elif i==4: xx=4
         
         my_fitting = np.polyfit(scenario[TARGET_PODS], scenario[MEAN], xx, full=True)
         poly = np.poly1d(my_fitting[0])
@@ -191,13 +145,13 @@ def boxplot_state(data_type:str, calculation_type:str):
     # handles, labels = plt.gca().get_legend_handles_labels()
     # red_patch = mpatches.Patch(color='red', label='The red data')
     plt.legend()
-    result_image_dir = "result\{}\{}\{} state {}_{}.jpg"
-    plt.savefig(result_image_dir.format(data_by_day_folder,folder,calculation_type,data_type,data_by_day_folder))
+    result_image_dir = "result\\"+data_by_day_folder+"\\{} state {}_{} log.jpg"
+    plt.savefig(result_image_dir.format("pi4",data_type,data_by_day_folder))
     plt.show()
     # remove_optimize_file(data_optimize_file.format(data_type))
 
-def boxplot_action(data_type:str, calculation_type:str):
-    df = pd.read_csv(output_data_dir+"data_{}_{}_optimize_tmp.csv".format(data_type, calculation_type), sep=',', names=[TARGET_PODS, REPEAT_TIME, POD_STATE, MEAN])
+def boxplot_action(data_type:str):
+    df = pd.read_csv(output_data_dir+"data_{}_{}_optimize_tmp.csv".format(data_type, CALCULATION_TYPE), sep=',', names=[TARGET_PODS, REPEAT_TIME, POD_STATE, MEAN])
     # df = pd.DataFrame(arr_data,columns=[TARGET_PODS, REPEAT_TIME, POD_STATE, MEAN])
     df = df.sort_values(by=TARGET_PODS)
 
@@ -233,15 +187,15 @@ def boxplot_action(data_type:str, calculation_type:str):
 
     arr = []
     labels = []
-    if calculation_type == "life_circle":
-        arr = [df_deploy, df_scale_down, df_curl, df_delete]
-        labels = ["Action:Deploying", "Action:Scaling down", "Action:Curling", "Action:Force delete", ]
+    if CALCULATION_TYPE == "life_circle":
+        arr = [df_deploy, df_scale_down, df_curl,]
+        labels = ["Action:Deploying", "Action:Scaling down", "Action:Curling"]
     else:
-        arr = [df_warm_curl, df_cooling, df_update, df_warm_delete, df_cold_delete]
-        labels = ["Action:WarmCurling", "Action:Cooling", "Action:Updating", "Action:Warm deleting", "Action:Cold deleting"]
+        arr = [df_warm_curl, df_update, df_warm_delete, df_cold_delete]
+        labels = ["Action:WarmCurling", "Action:Updating", "Action:Warm deleting", "Action:Cold deleting"]
     print(arr[1], arr[2])
     i=0
-    color = ["red","violet", "green", "blue","orange" ,]
+    color = ["red","green", "blue","orange" ,]
 
     marker = ["v", ">", "s", "v", ">", "s", "v"]
 
@@ -270,33 +224,34 @@ def boxplot_action(data_type:str, calculation_type:str):
         plt.xticks(x_positions, ticks)
 
         plt.xlabel("Number of Pods")
-        plt.yscale("log",base=2)
-        plt.ylabel("{} action {} usage".format(folder,data_type))
+        # plt.yscale("log", base=10)
+        plt.ylabel("PI4 {} action {} usage Watts".format(CALCULATION_TYPE,data_type))
         
         if i==0:xx=1
         elif i==1: xx=1
         elif i==2: xx=2
-        # elif i==3: xx=3
-        # elif i==4: xx=4
         
-        my_fitting = np.polyfit(scenario[TARGET_PODS], scenario[MEAN], 1, full=True)
-        print(my_fitting)
+        my_fitting = np.polyfit(scenario[TARGET_PODS], scenario[MEAN], 3, full=True)
         poly = np.poly1d(my_fitting[0])
         plt.plot(scenario[TARGET_PODS].astype(str), poly(scenario[TARGET_PODS]), color=color[i],  marker=marker[i], label = labels[i])
 
         print(my_fitting[0])
         i = i + 1
 
+    # legend
+    # handles, labels = plt.gca().get_legend_handles_labels()
+    # red_patch = mpatches.Patch(color='red', label='The red data')
     plt.legend()
+    # result_image_dir = "result\\"+data_by_day_folder+"\\{} action {}_{} log.jpg"
+    # plt.savefig(result_image_dir.format("pi4",data_type,data_by_day_folder))
     result_image_dir = "result\{}\{}\{}_{}_action_{}_{}.jpg"
-    plt.savefig(result_image_dir.format(data_by_day_folder,calculation_type,folder,calculation_type,data_type,data_by_day_folder))
+    plt.savefig(result_image_dir.format(data_by_day_folder,CALCULATION_TYPE,"pi4",CALCULATION_TYPE,data_type,data_by_day_folder))
     plt.show()
-    remove_optimize_file(data_optimize_file.format(data_type, calculation_type))
+    remove_optimize_file(data_optimize_file.format(data_type, CALCULATION_TYPE))
 
+def format_time(dtime:str):
+    return datetime.datetime.strptime(dtime, "%Y-%m-%d %H:%M:%S.%f+07:00")
 if __name__=='__main__':
-    generate_optimize_data_files(data_directory+"\\"+data_by_day_folder, CALCULATION_TYPE)
-    # boxplot_state(CPU_PER)
-    # boxplot_state(RAM_PER)
-    boxplot_action(CPU_PER, CALCULATION_TYPE)
-    boxplot_action(RAM_PER, CALCULATION_TYPE)
-    # mean_plot(CPU_PER)
+    generate_optimize_data_files(power_data_directory+"\\"+power_with_status_folder)
+    # boxplot_state("power")
+    boxplot_action("power")
